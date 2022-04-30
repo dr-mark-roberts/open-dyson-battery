7de7dbe44d53ad6a191d5e2a3aa077f392d0853f
sheinz <2250775+sheinz@users.noreply.github.com>
2022-04-30 19:13:07 +0300

    Update make files to the latest MPLAB version
    Use MPLAB version: 6.00
    Use xc8 version: 2.36

--- fw/nbproject/Makefile-local-default.mk
+++ fw/nbproject/Makefile-local-default.mk
@@ -3,7 +3,7 @@
 #
 #
 # This file contains information about the location of compilers and other tools.
-# If you commmit this file into your revision control server, you will be able to 
+# If you commmit this file into your revision control server, you will be able to
 # to checkout the project and build it from the command line with make. However,
 # if more than one person works on the same project, then this file might show
 # conflicts since different users are bound to have compilers in different places.
@@ -12,26 +12,26 @@
 # least once so the file gets created and the project can be built. Finally, you can also
 # avoid using this file at all if you are only building from the command line with make.
 # You can invoke make with the values of the macros:
-# $ makeMP_CC="/opt/microchip/mplabc30/v3.30c/bin/pic30-gcc" ...  
+# $ makeMP_CC="/opt/microchip/mplabc30/v3.30c/bin/pic30-gcc" ...
 #
-PATH_TO_IDE_BIN=/opt/microchip/mplabx/v5.45/mplab_platform/platform/../mplab_ide/modules/../../bin/
+PATH_TO_IDE_BIN=/opt/microchip/mplabx/v6.00/mplab_platform/platform/../mplab_ide/modules/../../bin/
 # Adding MPLAB X bin directory to path.
-PATH:=/opt/microchip/mplabx/v5.45/mplab_platform/platform/../mplab_ide/modules/../../bin/:$(PATH)
+PATH:=/opt/microchip/mplabx/v6.00/mplab_platform/platform/../mplab_ide/modules/../../bin/:$(PATH)
 # Path to java used to run MPLAB X when this makefile was created
 MP_JAVA_PATH="/usr/lib/jvm/java-8-openjdk/jre/bin/"
 OS_CURRENT="$(shell uname -s)"
-MP_CC="/opt/microchip/xc8/v2.31/bin/xc8-cc"
+MP_CC="/opt/microchip/xc8/v2.36/bin/xc8-cc"
 # MP_CPPC is not defined
 # MP_BC is not defined
-MP_AS="/opt/microchip/xc8/v2.31/bin/xc8-cc"
-MP_LD="/opt/microchip/xc8/v2.31/bin/xc8-cc"
-MP_AR="/opt/microchip/xc8/v2.31/bin/xc8-ar"
-DEP_GEN=${MP_JAVA_PATH}java -jar "/opt/microchip/mplabx/v5.45/mplab_platform/platform/../mplab_ide/modules/../../bin/extractobjectdependencies.jar"
-MP_CC_DIR="/opt/microchip/xc8/v2.31/bin"
+MP_AS="/opt/microchip/xc8/v2.36/bin/xc8-cc"
+MP_LD="/opt/microchip/xc8/v2.36/bin/xc8-cc"
+MP_AR="/opt/microchip/xc8/v2.36/bin/xc8-ar"
+DEP_GEN=${MP_JAVA_PATH}java -jar "/opt/microchip/mplabx/v6.00/mplab_platform/platform/../mplab_ide/modules/../../bin/extractobjectdependencies.jar"
+MP_CC_DIR="/opt/microchip/xc8/v2.36/bin"
 # MP_CPPC_DIR is not defined
 # MP_BC_DIR is not defined
-MP_AS_DIR="/opt/microchip/xc8/v2.31/bin"
-MP_LD_DIR="/opt/microchip/xc8/v2.31/bin"
-MP_AR_DIR="/opt/microchip/xc8/v2.31/bin"
+MP_AS_DIR="/opt/microchip/xc8/v2.36/bin"
+MP_LD_DIR="/opt/microchip/xc8/v2.36/bin"
+MP_AR_DIR="/opt/microchip/xc8/v2.36/bin"
 # MP_BC_DIR is not defined
-DFP_DIR=/opt/microchip/mplabx/v5.45/packs/Microchip/PIC12-16F1xxx_DFP/1.2.63
+DFP_DIR=/opt/microchip/mplabx/v6.00/packs/Microchip/PIC12-16F1xxx_DFP/1.3.90
--- fw/nbproject/Makefile-variables.mk
+++ fw/nbproject/Makefile-variables.mk
@@ -8,6 +8,3 @@ CND_BASEDIR=`pwd`
 CND_ARTIFACT_DIR_default=dist/default/production
 CND_ARTIFACT_NAME_default=fw.production.hex
 CND_ARTIFACT_PATH_default=dist/default/production/fw.production.hex
-CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=fw.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/fw.tar
